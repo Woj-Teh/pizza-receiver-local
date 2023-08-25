1e3cd7b0e2121821686fa2bec805c8778d4d3826
Z6WOD <wod@kmd.dk>
2023-08-26 00:36:50 +0200

    dzialajaca apka z errorem- kiedy jest timeout

--- src/main/java/com/example/pizzareceiver/applicationController/PizzaReceiverController.java
+++ src/main/java/com/example/pizzareceiver/applicationController/PizzaReceiverController.java
@@ -8,16 +8,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @CrossOrigin
 @SpringBootApplication
 @RestController
 public class PizzaReceiverController {
 
 
+
     private static final Logger logger = LoggerFactory.getLogger(PizzaReceiverController.class);
 
     @PostMapping("/pizza-receiver")
-    public ResponseEntity<String> receivePizza(@RequestBody Pizza pizza) {
+    public ResponseEntity<String> receivePizza(@RequestBody Pizza pizza) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(10);
 
         // Use the logger to log the received pizza details
         logger.info("Received pizza:");
